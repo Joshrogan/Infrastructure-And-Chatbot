4385c636e03996db088cf1c2bb0c8e38ab167ffb
Joshua Rogan <joshua.rogan@mydit.ie>
2021-02-06 14:08:11 +0000

    added comment explaining webiste bucket ARN

--- lib/config/pipelineConfig.ts
+++ lib/config/pipelineConfig.ts
@@ -1,9 +1,12 @@
+// after registering a domain with R53 you can find this out by going to the R53 console
 export const hostedZoneId = 'Z0512565XSIINZ7IKJQ4'
 
 export const website_domain = 'velocityspa.net'
 
-export const websiteCertArn = 'arn:aws:acm:us-east-1:509079033231:certificate/31e37e2d-c9cb-4f42-a59d-0ad7a71a10f4'
+// this is generated from deploying the pipeline-certificate stack
+export const websiteCertArn = 'arn:aws:acm:us-east-1:509079033231:certificate/a9654e21-7015-4e32-90b8-5e4eaf4f54f7'
 
+// for static websites, these always follow the convention 'arn:aws:s3:::DOMAIN'
 export const websiteBucketArn = 'arn:aws:s3:::velocityspa.net'
 
 export const usEast1 = { account: '509079033231', region: 'us-east-1' }
