ff674ad8f238b04f7ae3bca6635eb42b72445e67
Joshua Rogan <joshua.rogan@mydit.ie>
2021-02-06 13:42:22 +0000

    finished porting over basic pipeline code

---
+++ bin/pipeline.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { DomainCertificateStack } from '../lib/pipeline/domain-certificate';
+import { CloudfrontStack } from '../lib/pipeline/cloudfront';
+import { CodePipelineStack } from '../lib/pipeline/pipeline';
+import { dublin, usEast1 } from '../lib/config/pipelineConfig';
+
+const app = new cdk.App();
+
+// 1. setup domain certs with route53 and ACM
+new DomainCertificateStack(app, 'pipeline-certificate', { env: usEast1 })
+
+// 2. initalize a bucket that will act as cloudfront origin, setup to host static site & error pages.
+new CloudfrontStack(app, 'pipeline-cloudfront', { env: dublin })
+
+// 3. pipeline to take source from github, run through codebuild and output to above bucket.
+new CodePipelineStack(app, 'pipeline-codepipeline', { env: dublin })
---
+++ lib/pipeline/pipeline.ts
@@ -0,0 +1,73 @@
+import { BuildSpec, ComputeType, LinuxBuildImage, PipelineProject } from '@aws-cdk/aws-codebuild';
+import { Artifact, Pipeline } from '@aws-cdk/aws-codepipeline';
+import { CodeBuildAction, GitHubSourceAction, S3DeployAction } from '@aws-cdk/aws-codepipeline-actions';
+import { Bucket } from '@aws-cdk/aws-s3';
+import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
+import { RemovalPolicy } from '@aws-cdk/core';
+import {  websiteBucketArn } from '../config/pipelineConfig';
+
+export class CodePipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    const websiteBucket = Bucket.fromBucketArn(this, 'reactWebsiteBucket', websiteBucketArn)
+
+    const reactBuildProject = new PipelineProject(this, 'reactCodeBuild', {
+        buildSpec: BuildSpec.fromSourceFilename('buildspec.yml'),
+        environment: {
+            buildImage: LinuxBuildImage.STANDARD_5_0,
+            computeType: ComputeType.SMALL
+        }
+    })
+
+    const artifactBucket = new Bucket(this, 'artifactBucket', {
+        bucketName: 'velocity-pipeline-artifact-bucket',
+        removalPolicy: RemovalPolicy.DESTROY
+    })
+
+    const sourceArtifact = new Artifact('reactRepoLatestMain')
+
+    const buildArtifact = new Artifact('reactBuildOutputArtifact')
+
+
+    new Pipeline(this, 'reactPipeline', {
+        artifactBucket,
+        pipelineName: 'reactClientPipeline',
+        stages: [
+            {
+                stageName: 'SourceCode',
+                actions: [
+                    new GitHubSourceAction({
+                        actionName: 'readLatestMasterCommit',
+                        branch: 'main',
+                        output: sourceArtifact,
+                        oauthToken: SecretValue.secretsManager('github-token'),
+                        owner: 'Joshrogan',
+                        repo: 'react-client'
+                    })
+                ]
+            },
+            {
+                stageName: 'Build',
+                actions: [
+                    new CodeBuildAction({
+                        actionName: 'buildReactApp',
+                        input: sourceArtifact,
+                        outputs: [buildArtifact],
+                        project: reactBuildProject
+                    })
+                ]
+            },
+            {
+                stageName: 'Deploy',
+                actions: [
+                    new S3DeployAction({
+                        actionName: 'DeployReactApp',
+                        input: buildArtifact,
+                        bucket: websiteBucket
+                    })
+                ]
+            }
+        ]
+    })
+  }
+}
