92868b0c7469ef198b1818a97ffc86fc1d49429c
Joshua Rogan <joshua.rogan@mydit.ie>
2021-02-06 13:17:19 +0000

    added certificate stack

---
+++ lib/config/pipelineConfig.ts
@@ -0,0 +1,10 @@
+export const hostedZoneId = 'Z0512565XSIINZ7IKJQ4'
+
+export const website_domain = 'velocityspa.net'
+
+export const websiteCertArn = 'arn:aws:acm:us-east-1:509079033231:certificate/31e37e2d-c9cb-4f42-a59d-0ad7a71a10f4'
+
+export const websiteBucketArn = 'arn:aws:s3:::velocityspa.net'
+
+export const usEast1 = { account: '509079033231', region: 'us-east-1' }
+export const dublin = { account: '509079033231', region: 'eu-west-1' }
---
+++ lib/pipeline/certificate.ts
@@ -0,0 +1,24 @@
+import { DnsValidatedCertificate } from '@aws-cdk/aws-certificatemanager';
+import { HostedZone } from '@aws-cdk/aws-route53';
+import * as cdk from '@aws-cdk/core';
+import { CfnOutput } from '@aws-cdk/core';
+import { hostedZoneId, website_domain } from '../config/pipelineConfig';
+
+export class CertificateStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+      const hostedZone = HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+          hostedZoneId,
+          zoneName: website_domain
+      })
+  
+      const websiteCertificate = new DnsValidatedCertificate(this, 'DnsCert', {
+          domainName: website_domain,
+          hostedZone
+      })
+  
+      new CfnOutput(this, 'WebsiteCertARN', {
+          value: websiteCertificate.certificateArn
+      })
+    }
+  }
